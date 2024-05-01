fc8cdead7e23b4aaab156f28fb94d52ca3e5c6fd
lion <lionandall@gmail.com>
2024-05-01 09:30:11 +0300

    added home page

--- lion_chat/src/App.js
+++ lion_chat/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+
+import logo from './RRR.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="photo" src={logo} alt="Logo" />
+        <h1 >Welcome to My React App lion's chat!</h1>
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a sample React application showcasing basic features.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-buttons">
+          <button className="App-button" href="#about-us">
+            About Us
+          </button>
+          <button className="App-button" href="#services">
+            Services
+          </button>
+          <button className="App-button"  href="#contact">
+            Contact
+          </button>
+        </div>
       </header>
     </div>
   );
